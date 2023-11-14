e50381b3d55a82c29fd9204361c3bccc08300249
Bao Vo <giabaovo123456@gmail.com>
2023-11-15 02:52:14 +0700

    Add redis cache for use rate limit in production

--- book_challenge/settings/settings_prod.py
+++ book_challenge/settings/settings_prod.py
@@ -11,3 +11,13 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse(database_url)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': os.environ.get('REDIS_URL'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
