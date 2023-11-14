fe654e4b29534e82b590636adbe2edc840fec179
Bao Vo <giabaovo123456@gmail.com>
2023-11-14 19:26:30 +0700

    Correct settings_prod file

--- book_challenge/settings/settings_prod.py
+++ book_challenge/settings/settings_prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse(database_url)
